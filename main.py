c2ee98d47eb53bda093ce6a3dc9566e3ced67f19
MadCat <madcat@MadCatdeMacBook-Pro.local>
2025-08-01 14:28:17 +0800

    test message source

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
+from pkg.platform.types import message as platform_message
+
+# from ..LangBot.pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
+# from ..LangBot.pkg.plugin.events import *  # 导入事件类
+# from ..LangBot.pkg.platform.types import message as platform_message
 
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(BasePlugin):
+@register(name="DifyInputExt", description="Plugin for extend Dify input, sending more chat contexts", version="0.1", author="MadCat")
+class DifyInputExt(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
@@ -14,36 +19,33 @@ class HelloPlugin(BasePlugin):
     async def initialize(self):
         pass
 
+    # @handler(PromptPreProcessing)
+    # async def prompt_pre_processing(self, ctx: EventContext):
+
     # 当收到个人消息时触发
+
     @handler(PersonNormalMessageReceived)
     async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        msg_chain: platform_message.MessageChain = ctx.event.message_chain
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+        ctx.event.query["source"] = str(msg_chain.source)
 
     # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+    # @handler(GroupNormalMessageReceived)
+    # async def group_normal_message_received(self, ctx: EventContext):
+    #     msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
+    #     if msg == "hello":  # 如果消息为hello
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+    #         # 输出调试信息
+    #         self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
 
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
+    #         # 回复消息 "hello, everyone!"
+    #         ctx.add_return("reply", ["hello, everyone!"])
 
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+    #         # 阻止该事件默认行为（向接口获取回复）
+    #         ctx.prevent_default()
 
     # 插件卸载时触发
+
     def __del__(self):
         pass
